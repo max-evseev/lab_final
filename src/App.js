1a35c78219eafe9b4b6c694bc9887d0605c94991
Максим Євсеєв <MAKSYM.YEVSEIEV@lnu.edu.ua>
2025-05-04 15:41:02 +0300

    implemented search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 ﻿import Movie_list from './components/Movie_list'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 var all_movies = require('./data/movies.json');
     export default function App() {
     const [movie_list, set_list] = useState(all_movies);
+    const [search_query, set_query] = useState('');
+        useEffect(() => {
+        let query_result = [];
+            all_movies.forEach((movie) => {
+                if (movie.display_name.toLowerCase().search(search_query.toLowerCase().trim()) === 0) {
+                query_result.push(movie);
+                }
+            });
+        set_list(query_result);
+        }, [search_query]);
         return (
-        <div className="main_content">
-            {movie_list.length === 0 && <p>По вашому запиту нічого не знайдено</p>}
-            {movie_list.length !== 0 && <Movie_list movies={movie_list}></Movie_list>}    
-        </div>
+        <>
+            <div className="search_section">
+                <div className="search_input">
+                    <img className="input_icon" src={require('./icons/search.png')} draggable="false" alt=""></img>
+                    <input className="search_input" placeholder="Шукайте фільми..." value={search_query} onChange={e => set_query(e.target.value)}></input>
+                </div>
+            </div>
+            <div className="main_content">
+                {movie_list.length === 0 && <p>По вашому запиту нічого не знайдено</p>}
+                {movie_list.length !== 0 && <Movie_list movies={movie_list}></Movie_list>}    
+            </div>
+        </>
         );
     }
